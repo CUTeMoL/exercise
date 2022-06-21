fbf2841bca6575823c377c2c0296054aa90d4e79
CUTeMoL <106860446+CUTeMoL@users.noreply.github.com>
2022-06-21 14:47:00 +0800

    更新了写入EXCEL的功能

--- arp-scan/arppingScan.py
+++ arp-scan/arppingScan.py
@@ -3,11 +3,12 @@ import psutil
 import IPy
 import re
 # 导入OUI信息
-with open("/home/lxw/oui.txt", "r", encoding='utf8') as f:
+with open("./oui.txt", "r", encoding='utf8') as f:
     oui_list = f.readlines()
 net_devices = {} # 字典存储网卡信息
 scan_range = {} # 记录网段信息
-scan_list = []
+scan_list = [] # 设备列表
+results = {} # 结果
 # 遍历出IPV4地址，存到net_devices
 for if_name in psutil.net_if_addrs():
     for i in range(len(psutil.net_if_addrs()[if_name])):
@@ -41,7 +42,7 @@ except KeyError:
     print("请输入正确的网卡名称 ")
     exit()
 print("\n正在扫描网段: ", scan_range[scan_device_name])
-# arping
+# arp ping
 ans, unans = srp(Ether(dst="ff:ff:ff:ff:ff:ff")/ARP(pdst=scan_ip_range), timeout=2)
 print("\n扫描完成, 等待分析结果………………")
 for ans in ans.res:
@@ -52,4 +53,26 @@ for ans in ans.res:
         if re.search(macAddr.upper(), oui):
             mac_oui = oui.split('\t', -1)[2].rstrip()
             print(f"\t{ans.answer.psrc}\t{ans.answer.src}\t{mac_oui}")
+            # 存入字典
+            results[ans.answer.psrc] = [ans.answer.src, mac_oui]
+# 写入EXCEL文件
+workbook = xlsxwriter.Workbook("./LAN-mac-oui.xlsx")
+worksheet = workbook.add_worksheet("data")
+row = 0
+col = 0
+# EXCEL格式定制
+worksheet.set_column(col, col, width=20)
+worksheet.set_column(col+1, col+1, width=25)
+worksheet.set_column(col+2, col+2, width=50)
+BOLD = workbook.add_format({"bold": True})
+# 写入数据
+worksheet.write_string(0, 0, "ip地址", BOLD)
+worksheet.write_string(0, 1, "MAC地址", BOLD)
+worksheet.write_string(0, 2, "网卡产商", BOLD)
+for key, value in results.items():
+    result_data = [key, value[0], value[1]]
+    worksheet.write_row(row+1, col, result_data)
+    row = row + 1
+workbook.close()
+print("已写入当前文件夹的LAN-mac-oui.xlsx文件")
 flag = input("输入任意键退出: ")
