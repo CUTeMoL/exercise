2bc17be66d06c68b781eaccb62ac6202c70382d1
lxw <linxw0228@outlook.com>
2022-07-14 11:20:40 +0800

    update dirDiff.py

--- py/dirDiff.py
+++ py/dirDiff.py
@@ -1,11 +1,12 @@
-import difflib
-file1 = input("对比文件1路径: ")
-file2 = input("对比文件2路径: ")
-file3 = input("生成diff.html路径: ")
-with open(file1, "r", encoding="utf8") as f1:
-    f1lines = f1.read().splitlines(True)
-with open(file2, "r", encoding="utf8") as f2:
-    f2lines = f2.read().splitlines(True)
-result_message = difflib.HtmlDiff().make_file(f2lines, f1lines)
-with open(file3, "w", encoding="utf8") as f3:
-    f3.write(result_message)
+import filecmp
+dir_left = "D:/soft"
+dir_right = "D:/CUTeMoL"
+print("左边的文件:",filecmp.dircmp(dir_left, dir_right).left_list)
+print("右边的文件:",filecmp.dircmp(dir_left, dir_right).right_list)
+print("左右共同存在的内容(不检验文件内容):", filecmp.dircmp(dir_left, dir_right).common)
+print("左边独有的:", filecmp.dircmp(dir_left, dir_right).left_only)
+print("右边独有的:", filecmp.dircmp(dir_left, dir_right).right_only)
+print("两边目录都存在的子目录", filecmp.dircmp(dir_left, dir_right).common_dirs)
+print("两边目录都存在的子文件", filecmp.dircmp(dir_left, dir_right).common_files)
+print("左右共同存在的内容,检查文件的内容", filecmp.dircmp(dir_left, dir_right).same_files)
+print("左右共同存在, 但内容不匹配的", filecmp.dircmp(dir_left, dir_right).diff_files)
