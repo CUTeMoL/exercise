4fe8fb865cc7a17377f2419a197a22039ad0f32f
CUTeMoL <linxw0228@outlook.com>
2022-08-06 16:09:21 +0800

    update findTruePid.py

--- py/findTruePid.py
+++ py/findTruePid.py
@@ -4,6 +4,7 @@ import re
 
 process_name = "QQ" # 例如定义要查询的进程名为QQ
 process_dicts = {} # 空字典
+
 for pid in psutil.pids():
     # 正则匹配名为process_name定义的进程，加入字典
     if re.search(".*{}.*".format(process_name), psutil.Process(pid).name()):
@@ -13,7 +14,9 @@ for connection in psutil.net_connections():
     # 如过connection的pid在进程keys中则表示pid对应上了，删除这个key
     if connection.pid in process_dicts.keys():
         del process_dicts[connection.pid]
+
 # 打印没匹配到的字典，里面剩下的就是卡死进程的真实的pid
-print("pid process ppid")
-print(process_dicts.items())
+print("pid\t\tprocess\t\t\tppid")
+for key, value in process_dicts.items():
+    print(f"{key}\t\t{value[0]}\t\t{value[1]}")
 
