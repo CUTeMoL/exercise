f07b7a84a0045d94abf71208e0035bf0776912f0
nd_lxw <linxw0228@outlook.com>
2022-10-20 07:41:52 +0800

    update some files

--- py/ftp_upload.py
+++ py/ftp_upload.py
@@ -16,11 +16,16 @@ def ftp_upload(ftp_object, sourcefile, targetdir="/"):
     sourcefile_object = open(sourcefile,'rb')
     source_file_basename = os.path.basename(sourcefile)
     targetdir = targetdir.strip()
-    if targetdir[-1] == "/":
-        ftp_object.storbinary(f"STOR {targetdir}{source_file_basename}", sourcefile_object)
-    else:
-        ftp_object.storbinary(f"STOR {targetdir}/{source_file_basename}", sourcefile_object)
-    sourcefile_object.close()
-
+    try:
+        if targetdir[-1] == "/":
+            ftp_object.storbinary(f"STOR {targetdir}{source_file_basename}", sourcefile_object)
+        else:
+            ftp_object.storbinary(f"STOR {targetdir}/{source_file_basename}", sourcefile_object)
+        return True
+    except:
+        return False
+    finally:
+        sourcefile_object.close()
 if __name__ == "__main__":
     ftp_upload(ftp_object , filename, targetdir)
+    ftp_object.quit()
