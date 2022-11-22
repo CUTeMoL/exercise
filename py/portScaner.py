ae5de2809cfbaa5864022d9e85bbb61a871c217e
CUTeMoL <linxw0228@outlook.com>
2022-11-22 21:51:52 +0800

    update some files

--- py/portScaner.py
+++ py/portScaner.py
@@ -21,31 +21,30 @@ def port_test(ip_addr, port):
 
 def process_run(ip_ports):
     test_ip, test_ports = ip_ports
-    with ThreadPoolExecutor(max_workers=5000) as tpool:
+    with ThreadPoolExecutor(max_workers=6000) as tpool:
         futures = [ tpool.submit(port_test, test_ip, test_port) for test_port in test_ports ]
         for future in futures:
             if future.result()[2] == 0:
-                print(f"{future.result()[0]}:{future.result()[1]} is open.")
+                print("{}:{} is open.".format(future.result()[0], future.result()[1]))
             # elif future.result()[2] == 10035:
             #     print(f"{future.result()[0]}:{future.result()[1]} is close.")
 
 
 if __name__ == "__main__":
     freeze_support() # windows下打包为exe需要
-    print(f"{__author__}最后一次修改于{__last_mod_date__}")
-    print(f"更新了：{__modify__}")
+    print("{}最后一次修改于{}".format(__author__, __last_mod_date__))
+    print("更新了：{}".format(__modify__))
     print("下一次可以更新的内容为, 1.线程的异常处理 2.引入Ipy实现网段的扫描")
     ip_addr = None
     while True:
-        ip_addr = input("Input [done] to start scan or input ipaddr: ")
-        if ip_addr == "done":
+        ip_addr = input("Input [ipaddress] to add the scan list until input [start] to run process: \n")
+        if ip_addr == "start":
             break
         else:
             
             if re.match("127\.([0-9]{1,3}\.){2}[0-9]{1,3}", ip_addr):
-                print("use netstat.")
+                print("use \"netstat -nao\" or \"netstat -natlp\".")
             elif re.match("([0-9]{1,3}\.){1,3}[0-9]{1,3}", ip_addr):
-                print(ip_addr)
                 ip_addrs.append(ip_addr)
             else:
                 print("\nPlease enter the correct ip address and try again")
@@ -60,4 +59,4 @@ if __name__ == "__main__":
     process_pool.join()
     end = time.time()
     print("cost %.2fs"%(end - start))
-    input("exit")
+    input("exit.")
