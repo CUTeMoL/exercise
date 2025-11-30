1e75318ecdff4460c2f1abb46912c9977def06c2
lxw <worker_lxw@163.com>
2025-12-01 06:09:26 +0800

    Lxw updated note/PostgreSQL/__pycache__/dependencies.cpython-310.pyc,note/PostgreSQL/__pycache__/exec_command.cpython-310.pyc,note/PostgreSQL/__pycache__/exec_error.cpython-310.pyc,note/PostgreSQL/__pycache__/file_processing.cpython-310.pyc,note/PostgreSQL/__pycache__/read_conf.cpython-310.pyc,note/PostgreSQL/__pycache__/string_convert.cpython-310.pyc,note/PostgreSQL/__pycache__/version_utils.cpython-310.pyc,note/PostgreSQL/install.py,shell/git_commit.sh on 2025-12-01 06:09:26+0800

--- shell/git_commit.sh
+++ shell/git_commit.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 cd /code/CUTeMoL/exercise/
 git add -A
-git diff --name-only HEAD |  paste -sd ',' | xargs -I {} git commit -a -m "lxw updated {} on `date  "+%Y-%m-%d %H:%M:%S%z"`"
+git diff --name-only HEAD |  paste -sd ',' | xargs -I {} git commit -a -m "Lxw updated {} on `date  "+%Y-%m-%d %H:%M:%S%z"`"
 git push origin
