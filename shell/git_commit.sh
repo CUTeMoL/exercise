60480be34cabfb392efd937378ea75d1b53c290b
CUTeMoL <worker_lxw@163.com>
2026-01-14 11:12:17 +0800

    docs(CUTeMoL): updated shell/git_commit.sh on 2026-01-14 11:12:17+0800.

--- shell/git_commit.sh
+++ shell/git_commit.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 cd /code/CUTeMoL/exercise/
 git add -A
-git diff --name-only HEAD |  paste -sd ',' | xargs -I {} git commit -a -m "docs(`git config --get user.name`): updated {} on `date  "+%Y-%m-%d %H:%M:%S%z"`"
+git diff --name-only HEAD |  paste -sd ',' | xargs -I {} git commit -a -m "docs(`git config --get user.name`): updated {} on `date  "+%Y-%m-%d %H:%M:%S%z"`."
 git push origin
